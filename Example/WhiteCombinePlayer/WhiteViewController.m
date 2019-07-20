6b827e78e0d1f333c2ccf32d06fc807708190c4c
leavesster <11785335+leavesster@users.noreply.github.com>
2019-07-20 13:37:17 +0800

    add progress

--- Example/WhiteCombinePlayer/WhiteViewController.m
+++ Example/WhiteCombinePlayer/WhiteViewController.m
@@ -9,25 +9,37 @@
 #import "WhiteViewController.h"
 #import <WhiteCombinePlayer.h>
 #import <WhiteCombinePlayer/WhiteVideoView.h>
+#import <WhiteCombinePlayer/WhiteSliderView.h>
 #import <WhiteSDK.h>
 #import <Masonry.h>
 
 @interface WhiteViewController ()
+
+@property (nonatomic, strong) id timeObserver;
+
 @property (nonatomic, strong) WhiteBoardView *boardView;
 @property (nonatomic, strong) WhiteSDK *sdk;
 
 @property (nonatomic, strong) WhiteVideoView *videoView;
 @property (nonatomic, strong) WhiteCombinePlayer *combinePlayer;
 
+@property (nonatomic, strong) WhiteSliderView *progressView;
 @property (nonatomic, strong) UIToolbar *toolbar;
 
 @end
 
 @implementation WhiteViewController
-
 static NSString *const kM3u8 = @"https://d2zihajmogu5jn.cloudfront.net/sintel/master.m3u8";
-static NSString *const kUUID = @"b8343837f65d4f5c83925aa236ce543d";
-static NSString *const kRoomToken = @"WHITEcGFydG5lcl9pZD1OZ3pwQWNBdlhiemJERW9NY0E0Z0V3RTUwbVZxM0NIbDJYV0Ymc2lnPWUxNGQ0MzgyMjkzZGMyOGMwNWE4NjE2ZTBmN2QxNDY4MzkyZGRhNDE6YWRtaW5JZD0yMTYmcm9vbUlkPWI4MzQzODM3ZjY1ZDRmNWM4MzkyNWFhMjM2Y2U1NDNkJnRlYW1JZD0zNDEmcm9sZT1yb29tJmV4cGlyZV90aW1lPTE1OTQ3Mzk2NTkmYWs9Tmd6cEFjQXZYYnpiREVvTWNBNGdFd0U1MG1WcTNDSGwyWFdGJmNyZWF0ZV90aW1lPTE1NjMxODI3MDcmbm9uY2U9MTU2MzE4MjcwNjgzMzAw";
+static NSString *const kUUID = @"93e64389b1fb4ba58a0c1d50d9ad47b1";
+static NSString *const kRoomToken = @"WHITEcGFydG5lcl9pZD1OZ3pwQWNBdlhiemJERW9NY0E0Z0V3RTUwbVZxM0NIbDJYV0Ymc2lnPThmYWJkYTVmMjQyNjIyZGViYmI3ZjNhMDk5YmU1ZTgzYWYzYTc3OWU6YWRtaW5JZD0yMTYmcm9vbUlkPTkzZTY0Mzg5YjFmYjRiYTU4YTBjMWQ1MGQ5YWQ0N2IxJnRlYW1JZD0zNDEmcm9sZT1yb29tJmV4cGlyZV90aW1lPTE1OTUwOTM2MzUmYWs9Tmd6cEFjQXZYYnpiREVvTWNBNGdFd0U1MG1WcTNDSGwyWFdGJmNyZWF0ZV90aW1lPTE1NjM1MzY2ODMmbm9uY2U9MTU2MzUzNjY4MzQ4NzAw";
+
+- (void)dealloc
+{
+    if (self.timeObserver) {
+        [self.combinePlayer.videoPlayer removeTimeObserver:self.timeObserver];
+    }
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -38,11 +50,43 @@ static NSString *const kRoomToken = @"WHITEcGFydG5lcl9pZD1OZ3pwQWNBdlhiemJERW9NY
     [self.sdk createReplayerWithConfig:config callbacks:(id<WhitePlayerEventDelegate>)self completionHandler:^(BOOL success, WhitePlayer * _Nullable player, NSError * _Nullable error) {
         if (player) {
             self.combinePlayer = [[WhiteCombinePlayer alloc] initWithVideoUrl:[NSURL URLWithString:kM3u8] replayer:player];
+            // 设置视频显示 view
             [self.videoView setAVPlayer:self.combinePlayer.videoPlayer];
+            self.combinePlayer.delegate = (id<WhiteCombineDelegate>)self;
+            [self setupVideoProgress];
             // 提前 seek，replayer 会提前读取缓存，可以减少 play 时，bufferring 等待时间
             [player seekToScheduleTime:0];
+        } else {
+            NSLog(@"error:%@", [error description]);
         }
     }];
+}
+
+- (void)setupVideoProgress
+{
+    __weak typeof(self)weakSelf = self;
+    self.timeObserver = [self.combinePlayer.videoPlayer addPeriodicTimeObserverForInterval:CMTimeMake(1, 4) queue:dispatch_get_main_queue() usingBlock:^(CMTime time) {
+        if ([weakSelf.combinePlayer videoDuration]) {
+            weakSelf.progressView.value = CMTimeGetSeconds(time) / [weakSelf.combinePlayer videoDuration];
+        }
+    }];
+}
+
+#pragma mark - WhiteCombineDelegate
+- (void)combinePlayerPlayStateChange:(BOOL)isPlaying
+{
+    NSLog(@"play:%d", isPlaying);
+}
+
+- (void)loadedTimeRangeChange:(NSArray<NSValue *> *)loadedTimeRanges
+{
+//    NSLog(@"%s: %@", __FUNCTION__, [loadedTimeRanges description]);
+    CMTimeRange range = [[loadedTimeRanges firstObject] CMTimeRangeValue];
+    NSTimeInterval sec = CMTimeGetSeconds(range.start) + CMTimeGetSeconds(range.duration);
+    NSTimeInterval duration = [self.combinePlayer videoDuration];
+    if (duration && !isnan(sec)) {
+        self.progressView.bufferValue = sec / duration;
+    }
 }
 
 #pragma mark -
@@ -118,9 +162,10 @@ static NSString *const kRoomToken = @"WHITEcGFydG5lcl9pZD1OZ3pwQWNBdlhiemJERW9NY
     [self.view addSubview:self.toolbar];
     
     UIBarButtonItem *play = [[UIBarButtonItem alloc] initWithTitle:@"play" style:UIBarButtonItemStyleDone target:self action:@selector(play:)];
-    UIBarButtonItem *space = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    self.progressView = [[WhiteSliderView alloc] initWithFrame:CGRectMake(0, 0, 200, 40)];
+    UIBarButtonItem *progress = [[UIBarButtonItem alloc] initWithCustomView:self.progressView];
     UIBarButtonItem *pause = [[UIBarButtonItem alloc] initWithTitle:@"pause" style:UIBarButtonItemStyleDone target:self action:@selector(pause:)];
-    [self.toolbar setItems:@[play, space, pause]];
+    [self.toolbar setItems:@[play, progress, pause]];
         
     if (@available(iOS 11, *)) {
     } else {
